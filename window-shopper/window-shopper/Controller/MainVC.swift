7cd93639bf5f2fac901ccca621e58b25111e2948
Akhmet <ahmetoryn@gmail.com>
2018-03-05 13:52:56 +0600

    added calculate button over the keyboard

--- window-shopper/window-shopper/Controller/MainVC.swift
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -10,14 +10,26 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+    @IBOutlet weak var priceTxt: CurrencyTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+        calcBtn.setTitle("Посчитать", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @objc func calculate() {
+        print("Uraaaaa!")
     }
 
 
