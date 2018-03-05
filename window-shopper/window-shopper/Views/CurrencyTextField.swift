4d038c22aa814e6bfdd9759d5bd7a7a13e6376a4
Akhmet <ahmetoryn@gmail.com>
2018-03-05 13:15:03 +0600

    added IBDesignable

--- window-shopper/window-shopper/Views/CurrencyTextField.swift
+++ window-shopper/window-shopper/Views/CurrencyTextField.swift
@@ -8,19 +8,31 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTextField: UITextField {
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+    }
+
+    func customizeView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        
+        
         if placeholder == nil {
             placeholder = " "
         }
         let place = NSAttributedString(string: placeholder!,
-                        attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+                                       attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
         attributedPlaceholder = place
         textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     }
-
+    
 }
